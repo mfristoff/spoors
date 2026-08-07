86ad7aa6de05e48b14c3df371c8fb62293f59ad6
mfristoff <mike@olivemedia.agency>
2026-08-07 18:43:17 -0400

    mini split!

--- src/components/ui/Container 2.jsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export default function Container({ children, className, narrow }) {
-  return (
-    <div className={cn("mx-auto w-full max-w-[1280px] px-5 md:px-8", className)}>
-      {children}
-    </div>
-  );
-}
